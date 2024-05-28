6b15d5f69d500a497ea84350d9dfefbf37310408
TheRedHoodGuy <trhg.xavierokpalannajiaku.mez@gmail.com>
2024-05-28 11:33:37 +0100

    I can't Stress Responsiveness brev

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+window.addEventListener("scroll", function () {
+  const fadeInElements = document.querySelectorAll("fadeinOne");
+  fadeInElements.forEach(function (element) {
+    const elementPosition = element.getBoundingClientRect().top;
+    const viewportHeight = window.innerHeight;
+
+    // Check if element is in viewport
+    if (
+      elementPosition < viewportHeight &&
+      elementPosition >= -element.offsetHeight
+    ) {
+      element.classList.add("animate"); // Add class to trigger animation
+    } else {
+      element.classList.remove("animate"); // Remove class if not in viewport
+    }
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("header");
   const headerHeight = header.offsetHeight;
@@ -10,3 +28,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+function getCounting(val) {
+  alert("Iw");
+  thecount = 0;
+  for (i = 0; i <= val; i++) {
+    document.getElementById("count").innerHTML = thecount += 1;
+  }
+}
